9937202b625c4405531a28c417cb9fa5ef7de7ec
Emerson <emerson_10_s@hotmail.com>
2021-02-05 08:53:02 -0800

    test

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express ()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
+const produtos = [
+    {nome: "Maçã", preco:0.09},
+    {nome:"Doritos",preco:2.08},
+    {nome:"agua", preco:2}
+]
 
 app.get("/:nome/:lang",(req,res)=>{
     
@@ -10,7 +16,8 @@ app.get("/:nome/:lang",(req,res)=>{
     let lang = req.params.lang
     res.render("index", {
         nome,
-        lang
+        lang,
+        produtos
     })
 })
 
